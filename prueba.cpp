4357d523c0c6925200fa862bc4707a5482fa4df1
abelcastilla <37792315+abelcastilla@users.noreply.github.com>
2018-11-19 15:58:54 +0100

    modificacion feature

--- prueba.cpp
+++ prueba.cpp
@@ -2,11 +2,17 @@
 
 using namespace std;
 
+
+// esta feature añade una nueva funcinalidad a nuestro programa
+int lanzamientoDados(int dados){
+    return dados;
+}
+
 // este es un main de prueba para corroborar que tanto
 // la creacion de nuevos comentarios no afecta a una rama ya creada
-int main(){
+int main()
 
-    int a = 0;
+    int a = lanzamientoDados(0);
     // estos comentarios serviran para verificar a ciencia cierta
     // si realemente el git tiene la potencia suficiente como para evitarlos
     // sin ningun tipo de consecuencia negativa
