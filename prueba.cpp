238f98f10b85651ab1c78b0c004964c5bda8ae53
abelcastilla <37792315+abelcastilla@users.noreply.github.com>
2018-11-19 15:55:16 +0100

    rama master con comentarios

---
+++ prueba.cpp
@@ -0,0 +1,14 @@
+#include <isotream>
+
+using namespace std;
+
+// este es un main de prueba para corroborar que tanto
+// la creacion de nuevos comentarios no afecta a una rama ya creada
+int main(){
+
+    int a = 0;
+    // estos comentarios serviran para verificar a ciencia cierta
+    // si realemente el git tiene la potencia suficiente como para evitarlos
+    // sin ningun tipo de consecuencia negativa
+    return a;
+}
